06b0f81af86ed6f96f324e5940939a9a95051eff
bashmegus <bashmegus@gmail.com>
2024-04-17 20:49:51 +0300

    homework task 3 done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
+	double v, t, a, S;
+	cout << "Enter V than T than A: ";
+	cin >> v >> t >> a;
+	S = v * t + (a * (t * t)) / 2;
+	cout << "S equal to " << S << endl;
 	SetConsoleTextAttribute(hConsole, 7);
 	system("pause");
 	return 0;
