968705448029d7a076602bc2c4f1f4aafa171564
bashmegus <bashmegus@gmail.com>
2024-04-17 20:39:07 +0300

    homework task 1 done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	double R1, R2, R3;
+	cout << "Enter R1, R2 and R3: ";
+	cin >> R1 >> R2 >> R3;
 
+	double R0 = 1/(1/R1 + 1/R2 + 1/R3);
+
+	cout << "R0: " << R0 << endl;
 	SetConsoleTextAttribute(hConsole, 7);
 	system("pause");
 	return 0;
