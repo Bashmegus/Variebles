482df04204597b026134d5c1b6a88621692f64ac
bashmegus <bashmegus@gmail.com>
2024-04-17 20:07:30 +0300

    template changed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(hConsole, 7);
+	system("pause");
 	return 0;
 }
