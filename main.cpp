1118c06d98eabf81f83baf3fbe6e0d0d3f1b0196
bashmegus <bashmegus@gmail.com>
2024-04-17 20:43:57 +0300

    homework task 2 done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
+	float pi = 3.14;
+	double R;
+	cout << "Enter Circle Radius: ";
+	cin >> R;
+	double L = 2 * pi * R;
+	cout << "L is " << L << endl;
 	SetConsoleTextAttribute(hConsole, 7);
 	system("pause");
 	return 0;
