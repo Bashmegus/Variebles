438297bd58a371a572da9d80e7e9ff2baca7e071
bashmegus <bashmegus@gmail.com>
2024-04-22 19:06:19 +0300

    task 5 done

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	double scale, distancecm, distancekm;
-	cout << "Enter map scale(km): ";
-	cin >> scale;
-	cout << "Enter distance(cm): ";
-	cin >> distancecm;
-	distancekm = distancecm * scale;
-	cout << "Total distance between cities is equal to " << distancekm << endl;
-	system("pause");
-	return 0;
+    double Radius, V;
+    cout << "Enter radius of sphere: ";
+    cin >> Radius;
+    V = (4.0 / 3.0) * M_PI * Radius*Radius*Radius;
+    cout << "Volume of the sphere: " << V << endl;
+    system("Pause");
+    return 0;
 }
