3f3dd6a87821ca4cfb31139a987b3f2162050335
bashmegus <bashmegus@gmail.com>
2024-04-22 18:41:13 +0300

    task1 done

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
 int main() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	float pi = 3.14;
-	double R;
-	cout << "Enter Circle Radius: ";
-	cin >> R;
-	double L = 2 * pi * R;
-	cout << "L is " << L << endl;
-	SetConsoleTextAttribute(hConsole, 7);
+	int x;
+	cout << "Enter year: ";
+	cin >> x;
+	int days = 365 + (x % 4 == 0) - (x % 100 == 0) + (x % 400 == 0);
+	cout << "Amount of days in year " << x << " is " << days << endl;
 	system("pause");
-	return 0;
 }
