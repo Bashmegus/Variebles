d9908b89866b7db8131cb178401b174f50336308
bashmegus <bashmegus@gmail.com>
2024-04-22 19:12:10 +0300

    Hw task 1 done

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-    double Radius, V;
-    cout << "Enter radius of sphere: ";
-    cin >> Radius;
-    V = (4.0 / 3.0) * M_PI * Radius*Radius*Radius;
-    cout << "Volume of the sphere: " << V << endl;
+    int time, seconds, minutes, hours;
+    cout << "Enter time(in seconds): ";
+    cin >> time;
+    hours = time / 3600;
+    minutes = time / 60 - hours * 60;
+    seconds = time % 60;
+    cout << hours << " Hours, " << minutes << " Minutes, " << seconds << " Seconds" << endl;
     system("Pause");
     return 0;
 }
