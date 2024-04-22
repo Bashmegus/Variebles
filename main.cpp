5afd9117c00ed4bd3c88eca8863833a17fa2a911
bashmegus <bashmegus@gmail.com>
2024-04-22 19:16:50 +0300

    HW task 2 done

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int time, seconds, minutes, hours;
-    cout << "Enter time(in seconds): ";
-    cin >> time;
-    hours = time / 3600;
-    minutes = time / 60 - hours * 60;
-    seconds = time % 60;
-    cout << hours << " Hours, " << minutes << " Minutes, " << seconds << " Seconds" << endl;
+    double money;
+    int grn, cop;
+    cout << "Enter money: ";
+    cin >> money;
+    grn = money;
+    cop = (money - grn) * 100;
+    cout << grn << " grivnas, " << cop << " copiyok, " << endl;
     system("Pause");
     return 0;
 }
