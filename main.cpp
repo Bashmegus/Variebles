d47debb69b8a5b9363a63fead51cca5f2d84e424
bashmegus <bashmegus@gmail.com>
2024-04-22 18:48:44 +0300

    Task 2 done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
 int main() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	SetConsoleTextAttribute(hConsole, 7);
+	int grn, cop, calc;
+	cout << "Enter amount of grn then input amount of copiyok (format: 10 20): ";
+	cin >> grn >> cop;
+	calc = cop / 100;
+	grn += calc;
+	cop %= 100;
+	cout << "Corrected amount of grn and cop is: " << grn << " grn, " << cop << " cop!" << endl;
 	system("pause");
 	return 0;
 }
