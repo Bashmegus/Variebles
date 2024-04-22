da8c07ba522bc466298552a2c94a6b521da92157
bashmegus <bashmegus@gmail.com>
2024-04-22 19:30:29 +0300

    HW task 3 done

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    double money;
-    int grn, cop;
-    cout << "Enter money: ";
-    cin >> money;
-    grn = money;
-    cop = (money - grn) * 100;
-    cout << grn << " grivnas, " << cop << " copiyok, " << endl;
+    double distance, time, speed;
+    int minutes, seconds, secondsTime;
+    cout << "Enter distance and time: ";
+    cin >> distance >> time;
+    minutes = time;
+    seconds = (time - minutes) * 100;
+    secondsTime = seconds + minutes * 60;
+    speed = distance / secondsTime * 3.6;
+    cout << "speed is equal to " << setprecision(4) << speed << " km/h" << endl;
     system("Pause");
     return 0;
 }
