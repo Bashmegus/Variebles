4c792079a78da44144d63adbdec279f9596e37b8
bashmegus <bashmegus@gmail.com>
2024-04-24 19:38:02 +0300

    task 3 done

--- main.cpp
+++ main.cpp
@@ -1,62 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
-int calculateFuelConsumption(double weight) {
-    if (weight <= 500) {
-        return 1;
-    }
-    else if (weight <= 1000) {
-        return 4;
-    }
-    else if (weight <= 1500) {
-        return 7;
-    }
-    else if (weight <= 2000) {
-        return 9;
-    }
-    else {
-        return -1;
-    }
-}
-
 int main() {
-    double distanceAb, distanceBc, cargoWeight;
-    double fuelTankSize = 300;
+    int num1, num2, num3, num4, num5, num6, num7;
 
-    cout << "Distance between points A and B: ";
-    cin >> distanceAb;
-    cout << "Distance between points B and C: ";
-    cin >> distanceBc;
-    cout << "Weight of the cargo: ";
-    cin >> cargoWeight;
+    cout << "Enter 7 numbers: ";
+    cin >> num1 >> num2 >> num3 >> num4 >> num5 >> num6 >> num7;
 
-    double fuelConsumption = calculateFuelConsumption(cargoWeight);
+    int max = num1;
+    if (num2 > max) max = num2;
+    if (num3 > max) max = num3;
+    if (num4 > max) max = num4;
+    if (num5 > max) max = num5;
+    if (num6 > max) max = num6;
+    if (num7 > max) max = num7;
 
-    if (fuelConsumption == -1) {
-        cout << "Too heavy cargo" << endl;
-        return 0;
-    }
-
-    double abFuelRequired = distanceAb * fuelConsumption;
-    double totalFuelRequired = (distanceAb + distanceBc) * fuelConsumption;
-
-    if (abFuelRequired > fuelTankSize) {
-        cout << "Impossible to fly from point A to point B" << abFuelRequired << endl;
-        return 0;
-    }
-
-    double refuelRequired = totalFuelRequired - fuelTankSize;
-
-    if (refuelRequired > fuelTankSize) {
-        cout << "Imposible to fly from point B to point A" << endl;
-        return 0;
-    }
-    if (refuelRequired <= 0) {
-        cout << "Doesn't need refueling" << endl;
-        return 0;
-    }
-    cout << "Minimal refualing at point B is " << refuelRequired << endl;
-    
+    cout << "Maximum number: " << max << endl;
 
     return 0;
 }
