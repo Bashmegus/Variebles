8be7d0fe9a83ba8a95755333ba54ee99da722abe
bashmegus <bashmegus@gmail.com>
2024-04-24 18:51:01 +0300

    Task 2 done

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main() {
-	double d;
-	int i;
-	cin >> d;
-	i = d;
-	cout << i << endl;
+	int num1, num2;
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+	string resoult = (num1 > num2 ? "Num 1 bigger then num 2" : "Num 2 bigger then num 1");
+	if (num1 == num2) { resoult = "Numbers are equal";}
+	cout << resoult << endl;
 	system("pause");
 	return 0;
 }
