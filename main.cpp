4fc52774afd33b6ef5c712fda6db6eb990254fe7
bashmegus <bashmegus@gmail.com>
2024-04-24 19:10:15 +0300

    hw task 2 dobne

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int main() {
-    int number, n1, n2, n3, n4, n5, n6;
+    int number, n1, n2, n3, n4;
     cout << "Enter number: ";
     cin >> number;
-    if ((100000 <= number && number <= 999999)) {
-        n1 = number / 100000;
-        n2 = number / 10000 - n1 * 10;
-        n3 = number / 1000 - n1 * 100 - n2*10;
-        n4 = number / 100 - n1 * 1000 - n2*100 - n3*10;
-        n5 = number / 10 - n1 * 10000 - n2*1000 - n3*100 - n4*10;
-        n6 = number % 10;
-        (n1 + n2 + n3 == n4 + n5 + n6) ? cout << "Lucky number" << endl : cout << "Unlucky number" << endl;
+    if ((1000 <= number && number <= 9999)) {
+        n1 = number / 1000;
+        n2 = number / 100 - n1 * 10;
+        n3 = number / 10 - n1 * 100 - n2*10;
+        n4 = number % 10;
+        cout << n2 << n1 << n4 << n3 << endl;
     }
     else {
-        cout << "Error, number must be 6 numbers long" << endl;
+        cout << "Error, number must be 4 numbers long" << endl;
     }
 }
